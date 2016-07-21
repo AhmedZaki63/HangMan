bc8937bfdec1599e365010a56896eb7bffb1864a
AhmedHassaan <ahmedhassaan162@gmail.com>
2016-07-21 15:31:45 +0200

    Edit 1.5

--- app/src/main/java/com/example/lenovo/hangman/Keyboard.java
+++ app/src/main/java/com/example/lenovo/hangman/Keyboard.java
@@ -38,6 +38,20 @@ public class Keyboard extends Fragment {
                 if (keys[i])
                     keyboard_keys.get(i).setVisibility(View.INVISIBLE);
         }
+        String word=GameZaki.originalWord;
+        if(word.length()>5){
+            char hint1=GameZaki.hint1;
+            char hint2=GameZaki.hint2;
+            int n1=hint1-97,n2=hint2-97;
+            keyboard_keys.get(n1).setVisibility(View.INVISIBLE);
+            keyboard_keys.get(n2).setVisibility(View.INVISIBLE);
+        }
+        else{
+            char hint1=GameZaki.hint1;
+            int n1=hint1-97;
+            keyboard_keys.get(n1).setVisibility(View.INVISIBLE);
+        }
+
     }
 
     @Override
